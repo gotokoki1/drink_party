2f66256a63355959a7a0ba874c48940060298ef2
gotokoki1 <72792963+gotokoki1@users.noreply.github.com>
2021-03-29 15:20:12 +0900

    マイグレーションファイル作成

--- db/migrate/20210329061408_create_posts.rb
+++ db/migrate/20210329061408_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :text, null: false
+      t.text :content, null: false
+      t.date :party_time, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
