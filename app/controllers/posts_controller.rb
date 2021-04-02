b5106aeea2b7eb3c0530955fe8f825e879b4bdb5
gotokoki1 <72792963+gotokoki1@users.noreply.github.com>
2021-04-02 13:48:22 +0900

    create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,18 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text, :content).merge(user_id: current_user.id)
+  end
 
 end
